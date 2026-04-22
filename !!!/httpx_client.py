92919034ee73f53d0c9128f1b0faec5b3fbe4fd6
Vitaly Miller <vital.miller@gmail.com>
2026-04-21 22:09:43 -0700

    ✔️

---
+++ !!!/httpx_client.py
@@ -0,0 +1,33 @@
+"""
+HTTPX Client
+"""
+
+import httpx
+
+#=======================================================================================================================
+# 1. Проходим аутентификацию (обычный httpx)
+login_payload = {
+    'email': 'user@example.com',
+    'password': 'string'
+}
+# Выполняем httpx запрос
+login_response = httpx.post(
+    'http://localhost:8000/api/v1/authentication/login',
+    json=login_payload
+)
+print(login_response.json())       # {'token': {'tokenType': 'bearer', 'accessToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmUiOiIyMDI2LTA0LTIyVDA1OjMwOjIxLjYwMDI5MyIsInVzZXJfaWQiOiI3MjFjYjg4NS0xY2E4LTQ3OTEtYjA4YS03OGMxZGNhYTFiNmIifQ.juy1nlk004FXR7GE8laKPSBpHUrMdby-A3LlWid2In8', 'refreshToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHBpcmUiOiIyMDI2LTA2LTIxVDA1OjAwOjIxLjYwMDM4MSIsInVzZXJfaWQiOiI3MjFjYjg4NS0xY2E4LTQ3OTEtYjA4YS03OGMxZGNhYTFiNmIifQ.l_qD2jztuThll-cyS1IlRwiMeRIs8QNt4WOQok5EZFE'}}
+
+
+
+# 2. Инициализируем клиент с авторизацией (httpx.Client) 👈
+client = httpx.Client(
+    base_url='http://localhost:8000/api/v1',
+    timeout=100,
+    headers={'Authorization': f'Bearer {login_response.json()['token']['accessToken']}'}
+)
+
+# Выполняем client запрос с авторизацией
+get_user_me_response = client.get('/users/me')
+
+print(get_user_me_response.json())     # {'user': {'id': '721cb885-1ca8-4791-b08a-78c1dcaa1b6b', 'email': 'user@example.com', 'lastName': 'string', 'firstName': 'string', 'middleName': 'string'}}
+#-----------------------------------------------------------------------------------------------------------------------
