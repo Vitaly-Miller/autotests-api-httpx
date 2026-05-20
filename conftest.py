d1d18db83f8625ca2be2d3fbf16ee29d5b513f10
Vitaly Miller <vital.miller@gmail.com>
2026-05-19 22:14:38 -0700

    ✔️

--- conftest.py
+++ conftest.py
@@ -20,9 +20,9 @@ pytest_plugins = (
 #================================================ Plugins path (✨Auto) ================================================
 # Автоматически подключает все модули-файлы из папки fixtures/ (кроме __init__.py)
 # pytest_plugins = [
-#     f'fixtures.{f.stem}'
-#     for f in Path(__file__).parent.glob('fixtures/*.py')
-#     if f.name != '__init__.py'
+#     f'fixtures.{file.stem}'
+#     for file in Path(__file__).parent.glob('fixtures/*.py')
+#     if file.name != '__init__.py'
 # ]
 #=======================================================================================================================
 
