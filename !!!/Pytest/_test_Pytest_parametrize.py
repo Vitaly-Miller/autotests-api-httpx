62739c3d90a70ab5d92da656b6b847138a984b6a
Vitaly Miller <vital.miller@gmail.com>
2026-05-21 09:53:21 -0700

    ✔️

--- !!!/Pytest/_test_Pytest_parametrize.py
+++ !!!/Pytest/_test_Pytest_parametrize.py
@@ -39,7 +39,7 @@ def test_1(number: int):           # передаем <number>
     (2, 4),         # number = 2, expected = 4
     (3, 9)          # number = 3, expected = 9
 ])
-def test_2(number: int, expected: int):    # —>передаем <number> и <expected>
+def test_2(number: int, expected: int):    # —> передаем <number> и <expected>
     assert number ** 2 == expected  # подставляет КАЖДЫЙ (<number>, <expected>)
                                     # [1-1] PASSED
                                     # [2-4] PASSED
@@ -47,28 +47,28 @@ def test_2(number: int, expected: int):    # —>передаем <number> и <e
 
 #==================================== Multi Parametrize (Множественная параметризация) =================================
 # 12-in-1 (3x4)
-@pytest.mark.parametrize('host', [       # каждый параметр <host> будет подставлен 3 раза к Windows, MacOS и Linux
-    'https://host_1.com',
-    'https://host_2.com',
-    'https://host_3.com'
-    'https://host_4.com'
+@pytest.mark.parametrize('comp', [       # каждый параметр <comp> будет подставлен 3 раза к Windows, MacOS и Linux
+    'Computer-1',
+    'Computer-2',
+    'Computer-3'
+    'Computer-4'
 ])
-@pytest.mark.parametrize('os', [         # каждый параметр <OS> будет подставлен 4 раза - к host_1, _2, _3 и _4
+@pytest.mark.parametrize('os', [         # каждый параметр <OS> будет подставлен 4 раза - к computer_1, _2, _3 и _4
     'Windows',
     'MacOS',
     'Linux'
 ])
-def test_multi_param(host: str, os: str):   # <- передаем параметры
-    ...                                     # [Windows-https://host_1.com] PASSED
-                                            # [Windows-https://host_2.com] PASSED
-                                            # [Windows-https://host_3.com] PASSED
-                                            # [Windows-https://host_4.com] PASSED
-                                            # [MacOS-https://host_1.com] PASSED
-                                            # [MacOS-https://host_2.com] PASSED
-                                            # [MacOS-https://host_3.com] PASSED
-                                            # [MacOS-https://host_4.com] PASSED
-                                            # [Linux-https://host_1.com] PASSED
-                                            # [Linux-https://host_2.com] PASSED
-                                            # [Linux-https://host_3.com] PASSED
-                                            # [Linux-https://host_4.com] PASSED
+def test_multi_param(comp: str, os: str):   # <- передаем параметры
+    ...                                     # [Windows-Computer-1] PASSED
+                                            # [Windows-Computer-2] PASSED
+                                            # [Windows-Computer-3] PASSED
+                                            # [Windows-Computer-4] PASSED
+                                            # [MacOS-Computer-1] PASSED
+                                            # [MacOS-Computer-2] PASSED
+                                            # [MacOS-Computer-3] PASSED
+                                            # [MacOS-Computer-4] PASSED
+                                            # [Linux-Computer-1] PASSED
+                                            # [Linux-Computer-2] PASSED
+                                            # [Linux-hComputer-3] PASSED
+                                            # [Linux-Computer-4] PASSED
 #=======================================================================================================================
